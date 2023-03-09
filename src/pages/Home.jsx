a0f512084f1ca60a73ea5e6f2b83368a89cebd4a
Marck1js <marckk63prog@gmail.com>
2023-03-09 18:52:02 -0300

    page home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 
-export default function ProductsPage() {
+export default function Home() {
   return (
     <section className=''>
       <Header />
