4f64edc1b318b0e03cbde43f17a5b929efb84638
Marck1js <marckk63prog@gmail.com>
2023-03-09 18:11:47 -0300

    first commit

---
+++ src/svgComponents/Linkedin.jsx
@@ -0,0 +1,18 @@
+import * as React from "react"
+
+const Linkedin = (props) => (
+  <svg
+    width={18}
+    height={17}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M4.125 2.19c0 .935-.77 1.692-1.722 1.692S.68 3.125.68 2.19 1.45.5 2.403.5c.951 0 1.722.757 1.722 1.69Zm.014 3.044H.667v10.822h3.472V5.234Zm5.543 0h-3.45v10.822h3.45v-5.68c0-3.16 4.188-3.418 4.188 0v5.68h3.463V9.204c0-5.33-6.195-5.136-7.651-2.512V5.234Z"
+      fill="#1E1E1E"
+    />
+  </svg>
+)
+
+export default Linkedin
