b7e42a61cfce67ee0f2f171f382329138fc184f1
Elvio Galeano <elvio.galeano827@gmail.com>
2023-03-09 17:32:45 -0300

    product page component

---
+++ src/pages/ProductsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header'
+
+export default function ProductsPage() {
+  return (
+    <section className=''>
+      <Header />
+      <div className='h-screen flex justify-center items-center'>
+        <h1>PRODUCTOS</h1>
+      </div>
+    </section>
+  )
+}
