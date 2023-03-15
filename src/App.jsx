8ad64a1733f234b7bfc975ce941c694906e30776
PedroJS <fabricio_21_taurop@outlook.es>
2023-03-14 21:38:23 -0500

    Cart V1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
-import Login from './pages/Login'
-import Register from './pages/Register'
+import { useState } from "react";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Cart from "./pages/Cart";
+import ItemCart from "./components/ItemCart";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-      <Login/>
-      // <Register/>
-  )
+    <>
+      <Cart />
+      <div className="bg-red-200">S</div>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,48 +1,48 @@
-import React, { useState, useEffect } from 'react'
-import ph from '../../../../Imágenes/Emmeliel.jpg'
-import Google from '../svgComponents/Google'
-import Linkedin from '../svgComponents/Linkedin'
-import Facebook from '../svgComponents/Facebook'
-import x from './Login.module.css';
+import React, { useState, useEffect } from "react";
+import Google from "../svgComponents/Google";
+import Linkedin from "../svgComponents/Linkedin";
+import Facebook from "../svgComponents/Facebook";
+import x from "./Login.module.css";
 import { BiHide, BiShowAlt } from "react-icons/bi";
 
 const Login = () => {
   const [showPass, setShowPass] = useState(false);
 
   const [user, setUser] = useState({
-    username: '',
-    password: ''
+    username: "",
+    password: "",
   });
 
   const handleChange = (e) => {
-    const { name, value } = e.target
+    const { name, value } = e.target;
     setUser({
       ...user,
-      [name]: value
-    })
+      [name]: value,
+    });
   };
 
   useEffect(() => {
     if (user.password.length < 1) {
-      setShowPass(false)
+      setShowPass(false);
     }
   }, [user.password]);
 
   const handleFetch = async (e) => {
     e.preventDefault();
 
-    await fetch('http://localhost:8080/auth/login', {
-      method: 'POST',
+    await fetch("http://localhost:8080/auth/login", {
+      method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(user)
-    }).then(e => e.json()).then(e => {
-      localStorage.setItem('token', e.token)
-    });
-    setUser({...user, username: '', password: ''});
-
-  }
+      body: JSON.stringify(user),
+    })
+      .then((e) => e.json())
+      .then((e) => {
+        localStorage.setItem("token", e.token);
+      });
+    setUser({ ...user, username: "", password: "" });
+  };
 
   return (
     <div className="h-screen grid grid-cols-2">
@@ -61,7 +61,13 @@ const Login = () => {
             <p>Email</p>
             <div>
               <div className={x.allMailInput}>
-                <input onChange={(e) => handleChange(e)} type='text' placeholder='email@example.com' value={user.username} name='username' />
+                <input
+                  onChange={(e) => handleChange(e)}
+                  type="text"
+                  placeholder="email@example.com"
+                  value={user.username}
+                  name="username"
+                />
                 <i></i>
               </div>
             </div>
@@ -69,22 +75,28 @@ const Login = () => {
             <p>Contraseña</p>
             <div>
               <div className={x.allMailInput}>
-                <input onChange={(e) => handleChange(e)} type={showPass ? 'text' : 'password'} placeholder='yourPassword.1234' value={user.password} name='password' />
+                <input
+                  onChange={(e) => handleChange(e)}
+                  type={showPass ? "text" : "password"}
+                  placeholder="yourPassword.1234"
+                  value={user.password}
+                  name="password"
+                />
                 <div className={x.show} onClick={() => setShowPass(!showPass)}>
-                  {
-                    user.password < 1 ?
-                      null :
-                      showPass ?
-                        <BiShowAlt size={25} /> :
-                        <BiHide size={25} />
-                  }
+                  {user.password < 1 ? null : showPass ? (
+                    <BiShowAlt size={25} />
+                  ) : (
+                    <BiHide size={25} />
+                  )}
                 </div>
               </div>
             </div>
 
             <div className={x.check}>
               <div className={x.remember}>
-                <div className={x.checkbox}><input type='checkbox' /></div>
+                <div className={x.checkbox}>
+                  <input type="checkbox" />
+                </div>
                 <label htmlFor="">Recordar Contraseña</label>
               </div>
               <p>¿Olvidaste la contraseña?</p>
@@ -100,21 +112,27 @@ const Login = () => {
           </div>
 
           <ul className={x.social}>
-            <li><Google /></li>
-            <li><Facebook /></li>
-            <li><Linkedin /></li>
+            <li>
+              <Google />
+            </li>
+            <li>
+              <Facebook />
+            </li>
+            <li>
+              <Linkedin />
+            </li>
           </ul>
-          <p className={x.registro}>¿No tienes una cuenta? <span>Registrame</span></p>
+          <p className={x.registro}>
+            ¿No tienes una cuenta? <span>Registrame</span>
+          </p>
         </form>
-
       </div>
 
       <div className={x.right_side}>
         <p className={x.text}>Lorem Ipsum</p>
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
