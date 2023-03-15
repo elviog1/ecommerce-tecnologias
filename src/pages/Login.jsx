358522c336b0c1aef68708d927b0027120abc941
Marck1js <marckk63prog@gmail.com>
2023-03-15 10:39:48 -0300

    listo

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import ph from '../../../../Imágenes/Emmeliel.jpg'
 import Google from '../svgComponents/Google'
 import Linkedin from '../svgComponents/Linkedin'
 import Facebook from '../svgComponents/Facebook'
